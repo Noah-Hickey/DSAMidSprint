bc68d049ecd4c32806b2af4488ea8fb3976ea935
Noah-Hickey <Noah.Hickey@keyin.com>
2025-06-23 13:51:26 -0230

    Started task file, looks goos so far, may need minor changes in the future.

--- src/main/java/com/todo/Task.java
+++ src/main/java/com/todo/Task.java
@@ -1,4 +1,52 @@
 package com.todo;
 
 public class Task {
+    private final String title;
+    private final String description;
+    private boolean completed; // Completed or not //
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    // Getters & Setters //
+    public String setTitle(String title) {
+        return this.title;
+    }
+
+    public String setDescription(String description) {
+        return this.description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    // toString method //
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
 }
