4289742501c90361620e039c2f9274fe48703c24
Noah-Hickey <Noah.Hickey@keyin.com>
2025-06-24 17:25:39 -0230

    Added TaskList file and ensured sources root file was correctly marked.

--- src/main/java/com/todo/TaskList.java
+++ src/main/java/com/todo/TaskList.java
@@ -1,4 +1,53 @@
 package com.todo;
 
+import java.util.LinkedList;
+
 public class TaskList {
-}
+
+    private final LinkedList<Task> tasks;
+
+    // New empty TaskList constructor //
+    public TaskList(){
+        this.tasks = new LinkedList<>();
+    }
+
+    public void addTask(String title, String description){
+
+        tasks.add(new Task(title, description));
+        System.out.println("Task added: " + title + " - " + description);
+
+    }
+
+    public void markTaskAsCompleted(String title, String description) {
+       for (Task task : tasks){
+            if (task.getTitle().equals(title) && task.getDescription().equals(description)) {
+                task.markAsCompleted();
+                System.out.println("Task: " + title + "Description: " + description);
+                return;
+            }
+        }
+        System.out.println("Task: " + title + "Description: " + description + " not found in the list.");
+
+       }
+
+       public void printAllTasks() {
+           if (tasks.isEmpty()) {
+               System.out.println("This to-do list is empty.");
+               return;
+           }
+           System.out.println("--- To-Do List ---");
+           int taskNumber = 1;
+           for (Task task : tasks) {
+               System.out.println(taskNumber + ". " + task);
+               taskNumber++;
+           }
+           System.out.println("--------------------");
+       }
+
+    }
+
+
+
+
+
+
