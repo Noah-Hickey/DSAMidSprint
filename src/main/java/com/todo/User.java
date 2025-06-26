5339eba64f4158078dd213ae37f23b3b0e3387e7
Noah-Hickey <Noah.Hickey@keyin.com>
2025-06-25 22:04:03 -0230

    Completed user class.

--- src/main/java/com/todo/User.java
+++ src/main/java/com/todo/User.java
@@ -1,4 +1,42 @@
 package com.todo;
 
 public class User {
+
+    private String name;
+    private TaskList toDoList;
+
+    // Constructor //
+    public User(String name){
+        this.name = name;
+        this.toDoList = new TaskList();
+    }
+
+    // Getters & Setters //
+
+    public String setName(String name){
+        this.name = name;
+        return this.name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Other methods //
+
+    public void addTask(String title, String description){
+        toDoList.addTask(title, description);
+    }
+
+    public void markTaskAsCompleted(String title, String description){
+        this.toDoList.markTaskAsCompleted(title, description);
+    }
+
+    public void printTasks(){
+        System.out.println();
+        System.out.println("Tasks for " + this.name);
+        System.out.println("--------------------------------");
+        System.out.println(this.toDoList.toString());
+    }
+
 }
